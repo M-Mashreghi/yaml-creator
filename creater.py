4a0495107f0e703e959e82c4bfbf43c697e8c2c3
Mashreghi <amiramirmashgh@gmail.com>
2023-09-06 21:45:27 +0430

    w

--- creater.py
+++ creater.py
@@ -21,3 +21,9 @@ yaml_data = {
 # Save the YAML data to a file
 with open(r'H:\GIT project\yaml-creator\configurations.yaml', 'w') as yaml_file:
     yaml.dump(yaml_data, yaml_file)
+# your link goes here
+link = "https://github.com/M-Mashreghi/yaml-creator/blob/main/configurations.yaml"
+
+# note: this will break if a repo/organization or subfolder is named "blob" -- would be ideal to use a fancy regex
+# to be more precise here
+print(link.replace("github.com", "raw.githubusercontent.com").replace("/blob/", "/"))

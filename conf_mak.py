d52845e1179a728d0abe6d1d49895cbf2e54f356
Mashreghi <amiramirmashgh@gmail.com>
2023-09-06 22:02:02 +0430

    sd

--- conf_mak.py
+++ conf_mak.py
@@ -4,10 +4,8 @@ import yaml
 with open(r'H:\GIT project\yaml-creator\configurations.yaml', 'r') as yaml_file:
     yaml_data = yaml.safe_load(yaml_file)
 
-# Create a .conf file and write the data
+# Create a .conf file and write the URLs from YAML
 with open(r'H:\GIT project\yaml-creator\configurations.conf', 'w') as conf_file:
     for configuration in yaml_data["configurations"]:
-        url = configuration.get('url', 'default_url')  # Use 'url' or provide a default
-        conf_file.write(f"[{url}]\n")
-        conf_file.write(f"url={url}\n")
-        conf_file.write("\n")
+        url = configuration.get('url', '')  # Use 'url' or provide a default
+        conf_file.write(f"{url}\n")
--- creater.py
+++ creater.py
@@ -23,14 +23,14 @@ with open(r'H:\GIT project\yaml-creator\configurations.yaml', 'w') as yaml_file:
     yaml.dump(yaml_data, yaml_file)
 
 
-# Create a .conf file and write the data
-with open('configurations.conf', 'w') as conf_file:
-    for configuration in yaml_data["configurations"]:
-        conf_file.write(f"[{configuration['name']}]\n")
-        conf_file.write(f"url={configuration['url']}\n")
-        conf_file.write("\n")
+# # Create a .conf file and write the data
+# with open('configurations.conf', 'w') as conf_file:
+#     for configuration in yaml_data["configurations"]:
+#         conf_file.write(f"[{configuration['name']}]\n")
+#         conf_file.write(f"url={configuration['url']}\n")
+#         conf_file.write("\n")
 # your link goes here
-link = "https://github.com/M-Mashreghi/yaml-creator/blob/main/configurations.yaml"
+link = "https://github.com/M-Mashreghi/yaml-creator/blob/main/configurations.conf"
 
 # note: this will break if a repo/organization or subfolder is named "blob" -- would be ideal to use a fancy regex
 # to be more precise here

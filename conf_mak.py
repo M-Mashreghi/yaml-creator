c0797b5f33340ed37f5f765dc6e6e77c215cc577
Mashreghi <amiramirmashgh@gmail.com>
2023-09-06 22:23:21 +0430

    d

--- conf_mak.py
+++ conf_mak.py
@@ -8,4 +8,4 @@ with open(r'H:\GIT project\yaml-creator\configurations.yaml', 'r') as yaml_file:
 with open(r'H:\GIT project\yaml-creator\configurations.conf', 'w') as conf_file:
     for configuration in yaml_data["configurations"]:
         url = configuration.get('url', '')  # Use 'url' or provide a default
-        conf_file.write(f"{url}\n")
+        conf_file.write(f"{url}\n\n")
